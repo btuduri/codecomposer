08b264db9a84b249ae0a218123c2f0d3a6b59bfe
feeljuin <feeljuin@73b7253b-944e-0410-bf16-698164e7ef75>
2008-07-31 11:04:55 +0000

    채널별로 악기 소리

--- branches/MIDITest/arm9/source/smidlib_mtrk.h
+++ branches/MIDITest/arm9/source/smidlib_mtrk.h
@@ -7,8 +7,9 @@ extern void MTRKCC_Free(void);
 
 extern void MTRKCC_Proc(u32 trk,u8 data0,u8 data1);
 extern void MTRK_SetProgram(u32 trk,u32 v);
+extern u32 MTRK_GetProgram(u32 trk);
 extern void MTRK_ChangePitchBend(u32 trk,s32 p);
-extern void MTRK_NoteOn_LoadProgram(u32 trk,u32 Note,u32 Vel);
+extern void MTRK_NoteOn_LoadProgram(u32 trk,u32 Note,u32 program);
 extern void MTRK_NoteOn(u32 trk,u32 GT,u32 Note,u32 Vel);
 extern void MTRK_NoteOff(u32 trk,u32 Note,u32 Vel);
 
